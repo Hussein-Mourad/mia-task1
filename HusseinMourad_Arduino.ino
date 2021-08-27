0e07125a5baf8ccd54018a33b22226e3d7417399
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2021-08-27 06:02:55 +0200

    Changed decoding implementation to use pointers

--- HusseinMourad_Arduino.ino
+++ HusseinMourad_Arduino.ino
@@ -2,9 +2,10 @@
 short shift = 1;
 unsigned long time = 0;
 unsigned long blinkTime = 0;
-String password;
+char password[100];
+String str;
 
-String decrypt(String text, short shift);
+void decrypt(char *text, short shift);
 
 void setup()
 {
@@ -12,29 +13,30 @@ void setup()
   pinMode(ledPin, OUTPUT);
   Serial.println("\nSet up your password:");
   while (Serial.available() == 0) {};
-  password = Serial.readString();
+  str = Serial.readString();
   Serial.println("Password is set successfully.");
-
+  str.toCharArray(password, str.length()+1);
 }
 
 void loop()
 {
   Serial.println("Enter your password: ");
   while(Serial.available()==0){};
-  String inputPassword = Serial.readString();
+  str = Serial.readString();
   
-  if (shift == 25) {
-        shift = 1;
-  }
+  char inputPassword[str.length()];
+  str.toCharArray(inputPassword, str.length()+1);
   
-  inputPassword=decrypt(inputPassword, shift++);
+  decrypt(inputPassword, shift);
  
- 
-  if(inputPassword ==password){
+  if(strcmp(inputPassword,password)==0){
+    Serial.println("Correct password.");
+    shift++;
     digitalWrite(ledPin, HIGH);
     delay(3000);
   }
   else {
+    Serial.println("Incorrect password.");
     time = millis();
     blinkTime=millis();
     while (true){
@@ -42,22 +44,20 @@ void loop()
         blinkTime=millis();
         digitalWrite(ledPin, !digitalRead(ledPin));
       }
-      if(millis()-time>3000)break;
+      if(millis()-time>3000) break;
     }
   }
   digitalWrite(ledPin, LOW);
 }
 
 
-String decrypt(String text, short shift){
-  String result = "";
+void decrypt(char *text, short shift) {
+    const int length = strlen(text);
 
-  for (int i = 0; i < text.length(); i++) {
-    if (isupper(text[i]))
-      result += char(int(text[i] - shift - 65) % 26 + 65);
-    else
-      result += char(int(text[i] - shift - 97) % 26 + 97);
-  }
-  return result;
+    for (int i = 0; i < length; i++) {
+        if (isupper(*(text + i)))
+            *(text + i) = char(int(*(text + i) - shift - 65) % 26 + 65);
+        else
+            *(text + i) = char(int(*(text + i) - shift - 97) % 26 + 97);
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,6 @@
+# MIA task1
+
+## Acknowledgment
+
+I wasn't using pointers in arduino task as I couldn't get it right at that time. Now It figured it out so I decided to commit my changes.
+I would be so grateful if you could consider them.
